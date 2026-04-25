245ea66c1e6ac6f1cff7b20d3fdde496d58a27da
Molkiposik <defimov465@gmail.com>
2026-04-25 22:39:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -17,5 +17,5 @@ while True:
         break
     else:
         print("Нет! Попробуйте снова.")
-
+vless://443971e8-2620-4018-8988-75f5601dc02b@144.31.107.38:443?type=tcp&security=reality&fp=chrome&pbk=GM5JkBsl-MJBb0KAyzi96-AV3UCRY1kLFh-Os5qml2Q&sni=yandex.ru&sid=fa&spx=/&flow=xtls-rprx-vision#Бельгия-6 🇧🇪
 vless://0d3e3326-92ef-4f4e-892c-f3b8881e3d6c@5.129.220.189:443?type=tcp&security=reality&fp=chrome&pbk=l6BgSS_jH0QWHP7Fdm0JUymaBE3RN2kRwi7L-Xln0ws&sni=yandex.ru&sid=118f80&spx=/&flow=xtls-rprx-vision#Чехия-3 🇨🇿
