bd0e47d745955b2024357eaf9ca1f113097ea4a8
Molkiposik <defimov465@gmail.com>
2026-04-25 22:36:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -18,3 +18,4 @@ while True:
     else:
         print("Нет! Попробуйте снова.")
 
+vless://0d3e3326-92ef-4f4e-892c-f3b8881e3d6c@5.129.220.189:443?type=tcp&security=reality&fp=chrome&pbk=l6BgSS_jH0QWHP7Fdm0JUymaBE3RN2kRwi7L-Xln0ws&sni=yandex.ru&sid=118f80&spx=/&flow=xtls-rprx-vision#Чехия-3 🇨🇿
